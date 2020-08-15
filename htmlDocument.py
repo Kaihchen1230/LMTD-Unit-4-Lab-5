063754f4a0f3e96034fc701e85b87a609d0f5787
Kai Hang Chen <kaihang.c@gmail.com>
2020-08-15 13:57:46 -0400

    initilize a template html

--- htmlDocument.py
+++ htmlDocument.py
@@ -4,4 +4,19 @@ class HTML_Document:
     """
 
     def __init__(self):
+        self.html_document = {
+            "head": """
+                    <html>
+                        <head>
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+                        </head>
+                        <body>
+                  """,
+            "body": """<div class="alert alert-primary" role="alert">
+                        A simple primary alert—check it out!
+                       </div>""",
+            "end": """</body></html>"""
+        }
+
+    def add_to_body(self, tag, word):
         pass
